b9acddcc4d507a1c03ebd0617d7e6ae6108c9edc
NamHo <m8mi0909@gmail.com>
2025-10-06 20:17:38 +0700

    Tái cấu trúc đường dẫn thư mục src trong front-end

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,19 +1,10 @@
-import { useEffect, useState } from 'react';
-
-function App() {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    fetch('http://127.0.0.1:5000/') // Gọi backend
-      .then((res) => res.text())
-      .then((data) => setMessage(data));
-  }, []);
-
+import Button from '~/components/Button';
+const App = () => {
   return (
     <div>
-      <h1>Message from backend: {message}</h1>
+      <Button />
     </div>
   );
-}
+};
 
 export default App;
